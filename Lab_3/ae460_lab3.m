82a8fad6c714f6d17a943ab884fe85dc3748ec7c
alanh <alanh@LAPTOP-Q5SRV0PD>
2021-09-25 22:24:51 -0500

    Brushed up on plots

--- Lab_3/ae460_lab3.m
+++ Lab_3/ae460_lab3.m
@@ -26,7 +26,7 @@ p_e = table;
 rho_e = table;
 V_e = table;
 
-%%
+%% Jet Exit Analyses
 % Bernoulli
 p_e.Bernoulli = p_amb*ones(size(self.p_t));
 rho_e.Bernoulli = p_e.Bernoulli/R/T_amb;
@@ -40,16 +40,14 @@ V_e.isentropic = M_e.*sqrt(gamma*R*T_e);
 rho_e.isentropic = p_e.isentropic/R./T_e;
 
 % choked isentropic
-M_e(M_e > 1) = 1;
-T_e = T_amb./(1 + (gamma-1)/2*M_e.^2);
-V_e.choked = M_e.*sqrt(gamma*R*T_e);
-p_e.choked = self.p_t./(1 + (gamma-1)/2*M_e.^2).^(gamma/(gamma-1));
+M_1 = M_e;
+M_1(M_1 > 1) = 1;
+T_e = T_amb./(1 + (gamma-1)/2*M_1.^2);
+V_e.choked = M_1.*sqrt(gamma*R*T_e);
+p_e.choked = self.p_t./(1 + (gamma-1)/2*M_1.^2).^(gamma/(gamma-1));
 rho_e.choked = p_e.choked/R./T_e;
 
 Thrust = array2table((p_e{:,:}-p_amb+V_e{:,:}.^2.*rho_e{:,:})*A);
-
-psf2Pa = @(x) x*47.88;
-degR2K = @(x) x*5/9;
 
 V_e = varfun(@(x) x*0.3048, V_e);
 rho_e = varfun(@(x) x*515.37882, rho_e);
@@ -61,20 +59,82 @@ self.Load = self.Load*4.4482216;
 
 
 %%
-% figure(1), clf
-% hold on
-% plot(self.p_t, self.Load, 'LineStyle', 'none', 'LineWidth', 0.75,...
-%     'Marker', '^', 'MarkerSize', 5)
-% hold off
-% grid on
-% set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
-%     'XMinorTick', 'on', 'YMinorTick', 'on')
-% ax = gca;
-% ax.XAxis.LineWidth = 0.75;
-% ax.YAxis.LineWidth = 0.75;
+figure(1), clf
+hold on
+plot(self.p_t, M_e, 'LineStyle', '-', 'LineWidth', 0.75,...
+    'Marker', '^', 'MarkerSize', 5,...
+    'Color', 'blue', 'MarkerFaceColor', 'blue')
+plot(self.p_t(M_1>0.9), M_1(M_1>0.9), 'LineStyle', '--',...
+    'Color', 'blue', 'LineWidth', 0.75, 'Marker', '^', 'MarkerSize', 5)
+hold off
+grid on
+xlim([self.p_t(1), self.p_t(end)])
+xlabel('Chamber {\itp_t} (Pa)', 'FontSize', 12)
+ylabel('\itM_e', 'FontSize', 12)
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick', 'on')
+set(get(gca,'ylabel'), 'rotation', 0,...
+    'VerticalAlignment', 'middle', 'HorizontalAlignment', 'right')
+ax = gca;
+ax.XAxis.LineWidth = 0.75;
+ax.YAxis.LineWidth = 0.75;
+
+figure(2), clf
+hold on
+plot(self.p_t, V_e.Bernoulli, 'LineStyle', '-',...
+    'LineWidth', 0.75, 'Marker', '^', 'MarkerSize', 5,...
+    'Color', [0.1412, 0.5490, 0.0392],...
+    'MarkerFaceColor', [0.1412, 0.5490, 0.0392])
+plot(self.p_t, V_e.isentropic, 'LineStyle', '-',...
+    'LineWidth', 0.75, 'Marker', 'o', 'MarkerSize', 5,...
+    'Color', 'blue', 'MarkerFaceColor', 'blue')
+plot(self.p_t, V_e.choked, 'LineStyle', '-',...
+    'LineWidth', 0.75, 'Marker', 's', 'MarkerSize', 5,...
+    'Color', [0.9490, 0.4431, 0],...
+    'MarkerFaceColor', [0.9490, 0.4431, 0])
+hold off
+grid on
+xlim([self.p_t(1), self.p_t(end)])
+xlabel('Chamber {\itp_t} (Pa)', 'FontSize', 12)
+ylabel('{\itV_e} (m/s)', 'FontSize', 12)
+legend('Bernoulli', 'Isentropic', 'Isentropic + Choked' ,...
+    'Location', 'NorthWest')
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick', 'on')
+ax = gca;
+ax.XAxis.LineWidth = 0.75;
+ax.YAxis.LineWidth = 0.75;
 
 
+figure(3), clf
+hold on
+plot(self.p_t, self.Load, 'LineStyle', '-',...
+    'LineWidth', 0.75, 'Marker', '^', 'MarkerSize', 5,...
+    'Color', [0.1412, 0.5490, 0.0392],...
+    'MarkerFaceColor', [0.1412, 0.5490, 0.0392])
+plot(self.p_t, Thrust.Bernoulli, 'LineStyle', '--',...
+    'LineWidth', 0.75, 'Marker', '^', 'MarkerSize', 5,...
+    'Color', 'blue')
+plot(self.p_t, Thrust.isentropic, 'LineStyle', '--',...
+    'LineWidth', 0.75, 'Marker', 'o', 'MarkerSize', 5,...
+    'Color', [0.9490, 0.4431, 0])
+plot(self.p_t, Thrust.choked, 'LineStyle', '--',...
+    'LineWidth', 0.75, 'Marker', 's', 'MarkerSize', 5,...
+    'Color', [0.6902, 0.0431, 0.4118])
+hold off
+grid on
+xlim([self.p_t(1), self.p_t(end)])
+xlabel('Chamber {\itp_t} (Pa)', 'FontSize', 12)
+ylabel('Thrust (N)', 'FontSize', 12)
+legend('Experiment', 'Bernoulli', 'Isentropic', 'Isentropic + Choked' ,...
+    'Location', 'NorthWest')
+set(gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [.02,.02],...
+    'XMinorTick', 'on', 'YMinorTick', 'on')
+ax = gca;
+ax.XAxis.LineWidth = 0.75;
+ax.YAxis.LineWidth = 0.75;
 
+%% Functions
 function self = init(name, value)
     for n = 1:numel(name)
         self.(name{n}) = value(:,n);
